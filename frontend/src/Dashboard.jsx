c2617dc7464ccbc725a6f7f964aa9ecb1d3caa49
deoniloc742 <deoniloc742@gmail.com>
2025-12-04 18:53:37 +0800

    Tested tailwindcss if working, success

---
+++ frontend/src/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HelloWorld() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-400">
+      <h1 className="text-4xl font-bold text-gray-900">Hello, World! 👋</h1>
+    </div>
+  );
+}
