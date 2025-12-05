42e4446878cec64a5f12b06832a5d6bee369c4ff
deoniloc742 <deoniloc742@gmail.com>
2025-12-05 20:42:52 +0800

    2nd commit

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function HelloWorld() {
   return (
     <div className="h-screen flex items-center justify-center bg-gray-400">
-      <h1 className="text-4xl font-bold text-gray-900">Hello, World! 👋</h1>
+      <h1 className="font-bold text-gray-900 sm:text-xl md:text-2xl lg:text-5xl">Hello, World! 👋</h1>
     </div>
   );
 }
