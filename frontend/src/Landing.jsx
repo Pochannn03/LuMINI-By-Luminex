c809045567bbb264795493d596e3bf4f0326eb17
deoniloc742 <deoniloc742@gmail.com>
2025-12-09 17:21:24 +0800

    Login page, Installes Dependencies

--- frontend/src/Landing.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function HelloWorld() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-gray-400">
-      <h1 className="font-bold text-gray-900 sm:text-xl md:text-2xl lg:text-5xl">Hello, World! 👋</h1>
-    </div>
-  );
-}
