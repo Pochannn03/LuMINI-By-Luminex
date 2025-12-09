89e08ff83fd3395441759d532eec5d8a4fc2a768
deoniloc742 <deoniloc742@gmail.com>
2025-12-09 21:36:05 +0800

    Installed new depencies and added new pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Landing from './pages/Landing';
-import './styles/landing.css'
-import './styles/login.css'
-import Login from './pages/LogIn';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import './styles/landing.css';
+import './styles/login.css';
+import './styles/register.css';
+import App from './app';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
