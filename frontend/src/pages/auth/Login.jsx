696df3e70f761b29714097314c34b50b6ead70df
deoniloc742 <deoniloc742@gmail.com>
2025-12-12 01:33:45 +0800

    added file login,jsx and login.css

---
+++ frontend/src/pages/auth/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+
+
+export default function Login() {
+  <div className="min-h-screen w-full relative flex flex-col justify-end">
+
+  </div>
+}
