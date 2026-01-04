8e71327aaf5d8efa858f6b797a4f3cae0c80bd3c
deoniloc742 <deoniloc742@gmail.com>
2026-01-04 19:09:04 +0800

    Finished Navigations

--- frontend/src/components/navigation/SideNavBar.jsx
+++ frontend/src/components/navigation/SideNavBar.jsx
@@ -1,23 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import logo from '../../assets/lumini-logo.png'
+import Header from "./Header";
+import '../../styles/sidebar.css';
 
 export default function SideNavBar() {
-  return (
-    // <div className="dashboard-wrapper">
 
-    <aside className="sidebar">
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const isDesktop = window.innerWidth >= 1024;  
+    if (isDesktop && isOpen) {
+      document.body.classList.add("sidebar-open");
+    } else {
+      document.body.classList.remove("sidebar-open");
+    }
+  }, [isOpen]);
+
+  return (
+    <>
+    <Header onToggle={toggleMenu} />
+
+    <div 
+        className={`nav-overlay ${isOpen ? 'active' : ''}`} 
+        onClick={() => setIsOpen(false)}
+      >
+    </div>
+
+    <aside className={`sidebar ${isOpen ? 'expanded active' : ''}`}
+        id="sideNavBar"
+    >
 
       <div className="border-bottom flex shrink-0 items-center justify-center h-20 ">
-        <img src="../../assets/lumini-logo.png" alt="Lumini" className="sidebar-logo"></img>
+        <img src={logo} alt="Lumini" className="sidebar-logo"></img>
       </div>
 
       <nav className="sidebar-menu">
-        <Link to="#">
+
+        <Link to="#" className="nav-item active">
           <span className="material-symbols-outlined">dashboard</span>
           <span>Dashboard</span>
         </Link>
+
+        <Link to="#" className="nav-item">
+          <span className="material-symbols-outlined">school</span>
+          <span>Manage Classes</span>
+        </Link>
+
+        <Link to="#" className="nav-item">
+          <span className="material-symbols-outlined">analytics</span>
+          <span>Analytics</span>
+        </Link>
+
+        <Link to="#" className="nav-item">
+          <span className="material-symbols-outlined">manage_accounts</span>
+          <span>Accounts</span>
+        </Link>
+
+        <Link to="#" className="nav-item">
+          <span className="material-symbols-outlined">person</span>
+          <span>Profile</span>
+        </Link>
+
       </nav>
+
+      <div className="sidebar-footer">
+        <Link to="#" className="nav-item logout-btn">
+          <span className="material-symbols-outlined">logout</span>
+          <span>Sign Out</span>
+        </Link>
+      </div>
+
     </aside>
 
-    // </div>
+    </>
   );
 }
