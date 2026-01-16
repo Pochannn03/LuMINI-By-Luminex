33e55e3bda272123b5089bbd0092f7129adcc24d
deoniloc742 <deoniloc742@gmail.com>
2026-01-16 21:18:57 +0800

    renamed a file

--- frontend/src/components/modals/super-admin/class-management/ClassManageViewStudentModal.jsx
+++ frontend/src/components/modals/super-admin/class-management/ClassManageViewStudentModal.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-export default function ClassManageViewStudentProfileModal() {
+export default function ClassManageViewStudentModal() {
   return (
     <>
       {/* No Logic Yet soon to be implemented */}
