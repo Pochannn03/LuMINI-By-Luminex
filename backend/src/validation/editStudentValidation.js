eac94e20a5571d340bf6a0adfda8b471d9875371
deoniloc742 <deoniloc742@gmail.com>
2026-02-02 21:26:13 +0800

    Implemented Edit Student Information

---
+++ backend/src/validation/editStudentValidation.js
@@ -0,0 +1,22 @@
+export const updateStudentValidationSchema = {
+  first_name: {
+    optional: { options: { nullable: true, checkFalsy: true } }, // Allow empty/undefined
+    isLength: {
+      options: { min: 3, max: 32 },
+      errorMessage: "First Name must be 3-32 characters"
+    },
+  },
+  last_name: {
+    optional: { options: { nullable: true, checkFalsy: true } },
+    isLength: {
+      options: { min: 3, max: 32 },
+      errorMessage: "Last Name must be 3-32 characters"
+    },
+  },
+  birthday: {
+    optional: true,
+  },
+  age: {
+    optional: true,
+  },
+};
